63ed23a61a44a04896f0e652a64f702b7e3af26f
Kyungcheol <kcfigaro@gmail.com>
2019-02-11 22:17:50 +0000

    update maint.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,50 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-8fed39e1"
+data "aws_availability_zones" "all" {}
+
+resource "aws_elb" "example" {
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+    
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+    
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}/"
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+    
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-8fed39e1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
   
   user_data = <<-EOF
             #!/bin/bash
@@ -13,8 +53,8 @@ resource "aws_instance" "example" {
             nohup busybox httpd -f -p "${var.server_port}" &
             EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -27,6 +67,28 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
 }
 
 variable "server_port" {
@@ -34,6 +96,12 @@ variable "server_port" {
     default = 8080
 }
 
+/*
 output "public_ip" {
     value = "${aws_instance.example.public_ip}"
+}
+*/
+
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
